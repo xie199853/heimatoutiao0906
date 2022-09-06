c21e3f62229f465b0e82efe1d056791767665bbd
xwy1998 <11490343+xwy1998@user.noreply.gitee.com>
2022-09-06 18:12:28 +0800

    完成布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 Vue.use(VueRouter)
-
+// SAP
+// 首屏加载速度很慢
+// 路由懒加载
+// - 使用到了路由页面，再去请求他
 const routes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login')
+  }
 ]
 
 const router = new VueRouter({
