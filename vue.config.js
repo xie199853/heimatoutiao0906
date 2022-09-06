804cab3d8ed3a085a9a9f46130a6f84c85aada39
xwy1998 <11490343+xwy1998@user.noreply.gitee.com>
2022-09-06 11:35:30 +0800

    0906

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+const { resolve } = require('path')
+const cpns = resolve(__dirname, 'src/components')
+const views = resolve(__dirname, 'src/views')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  configureWebpack: {
+    devServer: {
+      port: 8888,
+      host: 'localhost',
+      open: true
+    },
+    resolve: {
+      alias: {
+        cpns,
+        views
+      }
+    }
+  }
 })
